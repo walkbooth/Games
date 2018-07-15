9ce4fb5a7ef090a268bef31ee33b5481a0e284c3
Walker Booth <walkbooth@gmail.com>
2018-07-15 15:19:22 -0400

    Feature for safe first pick and method for win condition.

--- src/grid.py
+++ src/grid.py
@@ -3,6 +3,11 @@ import random
 
 class Grid ():
 
+    """
+    Helper function to increment the bomb counter of a tile which has a bomb adjacent to it 
+    @param x the x coordinate of the bomb 
+    @param y the y coordinate of the bomb 
+    """
     def _notify_adj_tiles(self, x, y):
         # Iterate over all possible tiles which should be notified
         for rows in range(y-1, y+2):
@@ -15,17 +20,39 @@ class Grid ():
                         continue 
 
     """
+    Helper function to randomly generate valid locations for bombs based on game configuration. 
+    @param x the x coordinate of a 'safe tile' 
+    @param y the y coordinate of a 'safe tile'
+    @return bombs a list of valid locations for bombs to be placed in the grid 
+    """ 
+    def _generate_bomb_locations(self, x, y):
+        # Locate and place bombs, excluding first tile from the potential bomb spots 
+        valid_spots = set(range(0, self._width * self._height))
+        if not x == None and not y == None:
+            upper_left = y * self._width + x 
+            valid_spots -= set(range(upper_left, upper_left + 3) + 
+                               range(upper_left + self._width, upper_left + self._width + 3) + 
+                               range(upper_left + 2 * self._width, upper_left + 2 * self._width + 3))    
+
+        bomb_spots = random.sample(valid_spots, self._num_bombs)
+        return bomb_spots 
+
+    """
     Constructor for a grid object. Initializes all tiles in grid, and places bombs 
     randomly. 
     @param width the width of the grid
     @param height the height of the grid 
     @param num_bombs the number of bombs to place in the grid 
+    @param first_x the x coordinate of the first tile to reveal, guaranteed to be vacant. If not 
+                   provided, a full grid will be generated with no revealed tiles. 
+    @param first_y the y coordinate of the first tile to reveal, guaranteed to be vacant. If not 
+                   provided, a full grid will be generated with no revealed tiles.
     """
-    def __init__ (self, width, height, num_bombs):  
+    def __init__ (self, width, height, num_bombs, first_x = None, first_y = None):  
         
         # Validate arguments
         try:
-            if (num_bombs < 0 or num_bombs > width * height): 
+            if num_bombs < 0 or num_bombs > width * height: 
                 raise ValueError("Invalid arguments to create a grid.")
         except TypeError:
             raise ValueError("Invalid arguments to create a grid.")
@@ -34,6 +61,9 @@ class Grid ():
         self._grid_array = []
         self._width = width
         self._height = height 
+        self._revealed_tiles = 0
+        self._num_bombs = num_bombs 
+        self._uncovered_bomb = False
        
         # Iterate over grid, placing bombs and notifying tiles next to each bomb 
         for y in range(0, self._height):
@@ -41,8 +71,7 @@ class Grid ():
             for x in range(0, self._width):
                 self._grid_array[y].append(tile.Tile())
             
-        # Locate and place bombs 
-        bomb_spots = sorted(random.sample(range(0, self._width*self._height), num_bombs))
+        bomb_spots = self._generate_bomb_locations(first_x, first_y)
         for bomb_spot in bomb_spots:
             y = bomb_spot / height
             x = bomb_spot % height
@@ -66,6 +95,7 @@ class Grid ():
     def reveal_tile(self, x, y):
         uncovered_tile = self._get_tile(x, y)
         is_bomb = uncovered_tile.reveal()
+        self._revealed_tiles += 1 
 
         # Chain reveal if unveiled tile has no value 
         if not is_bomb and uncovered_tile.adj_bombs == 0:
@@ -78,6 +108,9 @@ class Grid ():
                                 self.reveal_tile(cols, rows)
                         except IndexError:
                             continue 
+        elif is_bomb:
+            self._uncovered_bomb = True 
+
         return is_bomb
 
     """
@@ -112,3 +145,15 @@ class Grid ():
         if x < 0 or x >= self._width or y < 0 or y >= self._height:
             raise IndexError("Indices provided are out of range")
         return self._grid_array[y][x]
+
+    """
+    Returns the state of the game at any given moment.
+    @returns 1 if game is won, -1 if game is lost (a bomb is revealed), 0 if game is in progress.
+    """
+    def game_state(self):
+        if self._uncovered_bomb:
+            return -1
+        elif self._revealed_tiles == self._width * self._height - self._num_bombs:
+            return 1
+        else:
+            return 0
--- test/test_grid.py
+++ test/test_grid.py
@@ -13,24 +13,30 @@ class testTile(unittest.TestCase):
     def test_constructor(self):
         grid1AllBomb = grid.Grid(1, 1, 1)
         self.assertEqual("* \n", grid1AllBomb.to_s())
+        self.assertEqual(1, grid1AllBomb.game_state())
         self.assertTrue(grid1AllBomb.reveal_tile(0, 0))
         self.assertEqual("B \n", grid1AllBomb.to_s())
+        self.assertEqual(-1, grid1AllBomb.game_state())
 
         grid4AllBomb = grid.Grid(2, 2, 4)
         self.assertEqual("* * \n* * \n", grid4AllBomb.to_s())
+        self.assertEqual(1, grid4AllBomb.game_state())
         self.assertTrue(grid4AllBomb.reveal_tile(1, 1))
         self.assertEqual("* * \n* B \n", grid4AllBomb.to_s())
+        self.assertEqual(-1, grid4AllBomb.game_state())
+
 
         grid1NoBomb = grid.Grid(1, 1, 0)
         self.assertEqual("* \n", grid1NoBomb.to_s())
         self.assertFalse(grid1NoBomb.reveal_tile(0, 0))
         self.assertEqual("_ \n", grid1NoBomb.to_s())
+        self.assertEqual(1, grid1NoBomb.game_state())
         
         grid4NoBomb = grid.Grid(2, 2, 0)
         self.assertEqual("* * \n* * \n", grid4NoBomb.to_s())
         self.assertFalse(grid4NoBomb.reveal_tile(0, 0))
         self.assertEqual("_ _ \n_ _ \n", grid4NoBomb.to_s())
-        
+        self.assertEqual(1, grid4NoBomb.game_state())        
 
         # Testing with bad parameters
         with self.assertRaises(ValueError):
@@ -47,14 +53,19 @@ class testTile(unittest.TestCase):
         random.sample._mock_side_effect = self.random.sample
         gridCustom1 = grid.Grid(3, 3, 3)
         self.assertEqual("* * * \n* * * \n* * * \n", gridCustom1.to_s())
+        self.assertEqual(0, gridCustom1.game_state())
         self.assertFalse(gridCustom1.reveal_tile(0, 0))
         self.assertEqual("_ _ _ \n2 3 2 \n* * * \n", gridCustom1.to_s())
+        self.assertEqual(1, gridCustom1.game_state())
         self.assertTrue(gridCustom1.reveal_tile(2, 2))
         self.assertEqual("_ _ _ \n2 3 2 \n* * B \n", gridCustom1.to_s())
+        self.assertEqual(-1, gridCustom1.game_state())
         self.assertTrue(gridCustom1.reveal_tile(1, 2))
         self.assertEqual("_ _ _ \n2 3 2 \n* B B \n", gridCustom1.to_s())
+        self.assertEqual(-1, gridCustom1.game_state())
         self.assertTrue(gridCustom1.reveal_tile(0, 2))
         self.assertEqual("_ _ _ \n2 3 2 \nB B B \n", gridCustom1.to_s())
+        self.assertEqual(-1, gridCustom1.game_state())
 
         gridCustom2 = grid.Grid(3, 3, 3)
         self.assertEqual("* * * \n* * * \n* * * \n", gridCustom2.to_s())
@@ -68,6 +79,8 @@ class testTile(unittest.TestCase):
         self.assertEqual("1 1 _ \n! 3 2 \n* * * \n", gridCustom2.to_s())
         self.assertFalse(gridCustom2.reveal_tile(0, 2))
         self.assertEqual("1 1 _ \n! 3 2 \n2 * * \n", gridCustom2.to_s())
+        self.assertEqual(1, gridCustom2.game_state())
+
 
         gridCustom3 = grid.Grid(3, 3, 3)
         self.assertTrue(gridCustom3.reveal_tile(1, 1))
@@ -88,14 +101,18 @@ class testTile(unittest.TestCase):
         self.assertFalse(gridCustom4.reveal_tile(2, 2))
         self.assertEqual("* 2 1 \n3 * 2 \n2 * 2 \n", gridCustom4.to_s())
 
-
-        
-
-
-
-
-
-
+        gridCustom5 = grid.Grid(4, 4, 4, 0, 0)
+        self.assertEqual("* * * * \n* * * * \n* * * * \n* * * * \n", gridCustom5.to_s())
+        self.assertFalse(gridCustom5.reveal_tile(0, 0))
+        self.assertEqual("_ _ 1 * \n_ _ 2 * \n1 1 4 * \n* * * * \n", gridCustom5.to_s())
+        self.assertEqual(0, gridCustom5.game_state())
+        self.assertFalse(gridCustom5.reveal_tile(3, 0))
+        self.assertEqual(0, gridCustom5.game_state())
+        self.assertFalse(gridCustom5.reveal_tile(2, 3))
+        self.assertEqual(0, gridCustom5.game_state())
+        self.assertFalse(gridCustom5.reveal_tile(0, 3))
+        self.assertEqual(1, gridCustom5.game_state())        
+        self.assertEqual("_ _ 1 1 \n_ _ 2 * \n1 1 4 * \n1 * 3 * \n", gridCustom5.to_s())
 
 if __name__ == '__main__':
     unittest.main()
